cf88bfebcc3689849969fd1097733e2b6bd90c29
Muhammad Fadhil Andavi <fadhillulussnbp@gmail.com>
2026-09-10 16:19:11 +0700

    Add files via upload

---
+++ PEKAN1/PROGRAM1_26115333012.py
@@ -0,0 +1,14 @@
+#program pertama
+"""
+komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+
+print("hello world")
+print()
+print("departemen informatika")
+print("universitas andalas")
+print("/ \\ // \\\\ /// \\\\\\")
+print("program ini" + "dibuat oleh a\\n"
+      + "Muhammad Fadhil Andavi 2611533012")
